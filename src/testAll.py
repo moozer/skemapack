f6a5b610fba46e1f685b3d33a5b9780faee9d189
Flindt <pfl@flindtathome.dyndns.org>
2010-10-15 14:23:47 +0200

    added folders

---
+++ src/testAll.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 15, 2010
+
+    Imports all unit tests from the test package
+
+@author: pfl
+'''
+import unittest
+
+
+import webimport.loadWebPage
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
