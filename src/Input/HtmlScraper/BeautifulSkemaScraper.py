d724cc04e1107d1fe610eed2d463857607faad40
morten <morten@localhost.localdomain>
2010-10-19 13:11:29 +0200

    Added BeautifulSkemaScraper

---
+++ src/Input/HtmlScraper/BeautifulSkemaScraper.py
@@ -0,0 +1,160 @@
+"""Extract list of URLs in a web page
+"""
+
+from BeautifulSoup import BeautifulSoup
+
+class BeautifulSkemaScraper( ):
+	def __init__(self, DateFormat = "%m/%d/%Y", TimeFormat = "%H:%M"):
+		""" Initialisation """
+		
+		self.DateFormat = DateFormat
+		self.TimeFormat = TimeFormat
+		self.WeekdayDate = ""
+		self.WeekDayCount = 0
+		self.LessonCount = 0
+		self.LessonContent = []
+		self.LessonHours = []
+		self.Appointments = []
+		
+	def feed( self, data ):
+		''' entry ser sxledes ud
+<div class="time">
+<ul>
+<li class="sHeader">Wednesday d.2/17/2010</li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">1</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">2 - 09:00 - 09:45</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain"><div class="sizer"><span class="sMainLektion">3 - 10:05 - 10:50</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">4 - 10:55 - 11:40</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">5</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">6</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">7</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">8</span> <div class="sMainContent">&nbsp;</div></div></li>
+</ul>
+</div>
+		'''
+		self.soup = BeautifulSoup( data );
+		for NewWeekDay in self.soup.findAll( 'div', {'class' : 'time'} ):
+			if not self.IncWeekDay( str( NewWeekDay.find( 'li', {"class": "sHeader"} ).contents[0] ) ):
+				continue;
+			
+			for NewLesson in NewWeekDay.findAll( 'div', {'class' : 'sizer'} ):
+				if not self.IncLesson( str( NewLesson.find( 'span', {"class": "sMainLektion"} ).contents[0] ) ):
+					continue;
+				
+				for NewLessonContent in NewLesson.findAll( 'div', {'class' : 'sMainContent'} ):
+					#if not self.IncLessonContent( NewLessonContent.contents ):
+					#	print "skipped", NewLessonContent.contents
+					self.IncLessonContent( NewLessonContent.contents )
+
+
+			
+	def IncWeekDay( self, data ):
+		""" Function that is called when new weekday is detected"""
+		if len( data ) <= 2:
+			return False
+
+		import datetime
+		self.WeekdayDate = datetime.datetime.strptime( str.strip( data ).split("d.")[1], self.DateFormat )
+		self.WeekDayCount += 1
+		self.LessonCount = 0
+		#print "New weekday: ", self.WeekDayCount, " with title: ", self.WeekdayDate
+		return True;
+		
+	def IncLesson( self, data ):
+		""" 
+		Function to be called when new lesson is encountered
+		
+		Saves the old lesson content and times as appointment, and starts to build the new one
+		"""
+		
+		if len( data ) <= 2:
+			return False
+			
+		if len( self.LessonContent ) > 0:
+			self.DumpLesson()
+		del self.LessonContent[:]
+		self.LessonCount += 1
+		
+		# convert hours to abosulte time using the current day as offset
+		import datetime
+		TmpSplit = str.split( data, " " )
+		del self.LessonHours[:]
+		for i in [2, 4]:
+			TmpTime = datetime.datetime.strptime( TmpSplit[i], self.TimeFormat )
+			TmpTimeSeconds = TmpTime.hour*60*60 + TmpTime.minute*60
+			self.LessonHours.append( datetime.timedelta( 0, TmpTimeSeconds )+self.WeekdayDate )
+			#print "lessonhours", self.LessonHours[-1]
+		return True
+
+		
+	def IncLessonContent( self, data ):
+		""" 
+		Function to be called when lesson content is encountered
+		
+		Save data for later use
+		"""
+		
+		# to avoid dummy entries
+		if len( data ) <= 2:
+			return False
+		
+		self.LessonContent = data
+		return True
+		
+	def DumpLesson( self ):
+		""" Appends current data as new appointment """
+		#print self.LessonContent
+		if len( self.LessonContent ) > 3:
+			self.Appointments.append( {	"Date": self.WeekdayDate, 
+										"Hours": [self.LessonHours[0], self.LessonHours[1]], 
+										"Subject": self.LessonContent[2].contents[0],
+										"Class": self.LessonContent[6].contents[0],
+										"Location": self.LessonContent[10].contents[0]
+									} )
+		else: # entries wth "BOOKED"
+			self.Appointments.append( {	"Date": self.WeekdayDate, 
+										"Hours": [self.LessonHours[0], self.LessonHours[1]], 
+										"Subject": unicode( self.LessonContent[2].contents[0], 'utf-8'),
+									} )
+		#print "lesson dump", self.Appointments[-1]
+	
+	def close( self ):
+		""" dummy function to be compatible"""
+		return
+
+def ProcessFile( FileToRead ):
+	""" Utility function. Reads the files and pipes it to SkemaScraper """
+	f = open( FileToRead, "rb" )
+	data = f.read();
+	parser = BeautifulSkemaScraper()
+	parser.feed(data)
+	parser.close()
+	f.close()
+	return  parser.Appointments 
+
+def ProcessWebPage( UrlToOpen = "http://skema.sde.dk/laerer/5421/en-US.aspx" ):
+	""" 
+	Utility function. Fetches webpage from the internet and parses it
+	"""
+	import urllib
+
+	usock = urllib.urlopen(UrlToOpen)
+	parser = BeautifulSkemaScraper()
+	parser.feed(usock.read())
+	parser.close()
+	usock.close()
+	return  parser.Appointments 
+	
+	
+if __name__ == "__main__":
+	app1 =ProcessWebPage( "http://skema.sde.dk/laerer/5421/en-US.aspx" )
+	#for a in app1:
+	#	print a["Date"], a["Hours"]
+
+	print app1
+	#app2 =ProcessWebPage( "http://skema.sde.dk/laerer/5421/da-DK.aspx" )
+	#for a in app2:
+	#	print a["Date"], a["Hours"]
+
---
+++ src/testpackage/Input/testHtmlScraper/TestBeautifulSoupScraper.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       untitled.py
+#       
+#       Copyright 2010  <morten@minimeee>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+from Input.HtmlScraper.BeautifulSkemaScraper import  BeautifulSkemaScraper
+import unittest
+import datetime
+
+# testdata
+SimpleSkemaData = """
+<div class="time">
+<ul>
+<li class="sHeader">Wednesday d.2/17/2010</li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">1</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">2 - 09:00 - 09:45</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain"><div class="sizer"><span class="sMainLektion">3 - 10:05 - 10:50</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">4 - 10:55 - 11:40</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="Netvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">5</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">6</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">7</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">8</span> <div class="sMainContent">&nbsp;</div></div></li>
+</ul>
+</div>
+"""
+SimpleSkemaDataResult = [
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 9, 0), datetime.datetime(2010, 2, 17, 9, 45)], 'Location': u'A-302', 'Class': u'10OIT2bH1', 'Subject': u'Netvxrk/OOP'}, 
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 10, 5), datetime.datetime(2010, 2, 17, 10, 50)], 'Location': u'A-302', 'Class': u'10OIT2bH1', 'Subject': u'Netvxrk/OOP'}, 
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 10, 55), datetime.datetime(2010, 2, 17, 11, 40)], 'Location': u'A-302', 'Class': u'10OIT2bH1', 'Subject': u'Netvxrk/OOP'}
+]
+
+SkemaDataBadChars = """
+<div class="time">
+<ul>
+<li class="sHeader">Wednesday d.2/17/2010</li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">1</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">2 - 09:00 - 09:45</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="æøåæøåNetvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302æøåæøå</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain"><div class="sizer"><span class="sMainLektion">3 - 10:05 - 10:50</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="æøåæøåNetvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2 æøåæøå">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+<li class="sMain"><div class="sizer"><span class="sMainLektion">4 - 10:55 - 11:40</span><div class="sMainContent"><strong>Fag:</strong> <acronym title="æøåæøåNetvxrk/OOP F2010">Netvxrk/OOP</acronym><br /><strong>Akt:</strong> <acronym title="IT 2. sem Network">10OIT2bH1æøåæøå</acronym><br /><strong>Lok:</strong> <acronym title="A-302 - Teorilokale          ET                   64,40 m2">A-302</acronym> <a href="/lokale/256/en-US.aspx" title="Gx til skema for lokale A-302">>></a><br /></div></div></li>
+
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">5</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">6</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">7</span> <div class="sMainContent">&nbsp;</div></div></li>
+<li class="sMain" ><div class="sizer"><span class="sMainLektion">8</span> <div class="sMainContent">&nbsp;</div></div></li>
+</ul>
+</div>
+"""
+SkemaDataBadCharsResult =  [
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 9, 0), datetime.datetime(2010, 2, 17, 9, 45)], 'Location': u'A-302æøåæøå', 'Class': u'10OIT2bH1', 'Subject': u'Netvxrk/OOP'}, 
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 10, 5), datetime.datetime(2010, 2, 17, 10, 50)], 'Location': u'A-302', 'Class': u'10OIT2bH1', 'Subject': u'Netvxrk/OOP'},
+    {'Date': datetime.datetime(2010, 2, 17, 0, 0), 'Hours': [datetime.datetime(2010, 2, 17, 10, 55), datetime.datetime(2010, 2, 17, 11, 40)], 'Location': u'A-302', 'Class': u'10OIT2bH1æøåæøå', 'Subject': u'Netvxrk/OOP'},
+    ]
+
+class TestInstantiations(unittest.TestCase):                            
+    def testEmptyData( self ):
+        """ use empty text in parser """
+        parser = BeautifulSkemaScraper()
+        parser.feed("")
+        parser.close()
+        self.assertEqual(parser.Appointments , [])                  
+
+    def testBogusTextData( self ):
+        """ use non-HTML text in parser """
+        parser = BeautifulSkemaScraper()
+        parser.feed("ThisHasNothingToDoWithHtml")
+        parser.close()
+        self.assertEqual(parser.Appointments , [])                  
+
+    def testNonTextData( self ):
+        """ use integer instead of text in parser """
+        parser = BeautifulSkemaScraper()
+        self.assertRaises(TypeError, parser.feed, 1)
+        parser.close()
+    
+    def testKnownText( self ):
+        """ use known data to get known result """
+        parser = BeautifulSkemaScraper()
+        parser.feed( SimpleSkemaData )
+        parser.close()
+        self.assertEqual(len( parser.Appointments ), len(SkemaDataBadCharsResult))
+        self.assertEqual(parser.Appointments , SimpleSkemaDataResult)  
+
+    def testKnownTextBadChars( self ):
+        """ use known data to get known result (including øæå) """
+        parser = BeautifulSkemaScraper()
+        parser.feed( SkemaDataBadChars )
+        parser.close()
+        self.assertEqual(len( parser.Appointments ), len(SkemaDataBadCharsResult))  
+        
+        for i in range(0, len( parser.Appointments ) ):
+            self.assertEqual(parser.Appointments[i] , SkemaDataBadCharsResult[i])  
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestInstantiations)
+    unittest.TextTestRunner(verbosity=3).run(suite)
+
+if __name__ == '__main__':
+    main()
