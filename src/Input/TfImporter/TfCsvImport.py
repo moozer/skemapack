9e5002f380646656cf7fb14c8deba1e80712798a
morten <morten@leon.lan>
2010-11-07 01:07:05 +0100

    basic import of tf from csv is now working no error handling tests implemented

--- src/Input/TfImporter/TfCsvImport.py
+++ src/Input/TfImporter/TfCsvImport.py
@@ -1,9 +1,11 @@
+# -*- coding: UTF-8 -*-
 
 '''
 Created on Nov 6, 2010
 
 @author: morten
 '''
+import csv
 
 class TfCsvImport():
     '''
@@ -15,15 +17,86 @@ class TfCsvImport():
         @param CsvInputFilename The file to retrieve data from.
         '''
         self._InputFile = CsvInputFilename
+        self._IsSearchEnabled = False
+ 
+        # needed for parsing 
+        # constants
+        self._NewClassKeywords = ["ANTAL STUDERENDE:"]
+        self._EndClassKeywords = ["I ALT", "I  ALT"]
+        self._CsvDelimiter = '\t'
+
+        self._InitSearchParams()
+ 
+    def _InitSearchParams(self):
+        ''' inits the variables needed in the search '''
+        self._state = 'FILEHEADER'
+        self._lineno = 0
+        self._classStartLine = 0
+        self._classEndLine = 0
+        self._CurrentClass = ""
+        self._CurrentTeacher = ""
+        self._CurrentCourse = ""
         
 # --- get/set functions --
     def getCsvFilename( self ):
         ''' returns the csv file specified for input '''
         return self._InputFile
     
+    def IsSearchEnabled(self):
+        ''' if true, system is ready to return entries '''
+        return self._IsSearchEnabled;
+    
 # --- other methods ---
     def EnableImportByTeacher( self, TeacherInitials ):
         '''
-        Searches the file for lecture based on a specific teacher
+        Reset the search for lecture based on a specific teacher
+        '''
+        self._InitSearchParams()
+        self._TeacherToSearchFor = TeacherInitials
+        self._TfReader = csv.reader(open(self._InputFile), delimiter=self._CsvDelimiter, quotechar='\"')
+        self._IsSearchEnabled = True
+        
+    def GetNextEntry( self ):
+        ''' 
+        based on the current search method, the next entry found is returned
+        returns empty list on eof
         '''
         
+        # needs error handling
+        for row in self._TfReader:
+
+            self._lineno += 1
+            if self._state in ['FILEHEADER',  'NEXTCLASS']:
+                if len(row) > 5 :
+                    if row[0] in self._NewClassKeywords :
+                        self._state = 'CLASSHEADER'
+                        self._classStartLine = self._lineno
+                    if  row[5] == "LÆRER":
+                        self._state = 'CLASSHEADER'
+                        self._classStartLine = self._lineno - 1
+            
+            if self._state == 'CLASSHEADER':
+                if self._lineno ==  self._classStartLine+1:
+                    self._CurrentClass = row[0]
+                if self._lineno - 2 > self._classStartLine :
+                    self._state = 'INCLASS'
+            
+            if self._state == 'INCLASS':
+                if row[0] in self._EndClassKeywords :
+                    self._state = 'CLASSFOOTER'
+                    self._classEndLine = self._lineno
+                else: 
+                    if len(row) > 5 :
+                        self._CurrentTeacher = row[5]
+                        self._CurrentCourse = row[0]
+            
+            if self._state == 'CLASSFOOTER':
+                if  self._lineno  > self._classEndLine:
+                    self._state = 'NEXTCLASS'
+
+            # if we have a match, return the line        
+            if self._state in ['INCLASS']:
+                if self._CurrentTeacher == self._TeacherToSearchFor:
+                    return {'Teacher': self._CurrentTeacher, 
+                            'Class': self._CurrentClass,
+                            'Course':   self._CurrentCourse }
--- src/testpackage/Input/testTfImporter/testTfImporter.py
+++ src/testpackage/Input/testTfImporter/testTfImporter.py
@@ -9,6 +9,19 @@ from Input.TfImporter.TfCsvImport import TfCsvImport
 # Test data
 TfInputCsvFile = "testdata/TF_skema.csv"
 
+Teacher1Initials = 'Teacher 7'
+Teacher1Classes = [     {'Course': 'Subject H1', 'Teacher': 'Teacher 7', 'Class': '1. Sem A Elektronik'},
+                        {'Course': 'Subject L1', 'Teacher': 'Teacher 7', 'Class': '1. Sem A Elektronik'},
+                        {'Course': 'Subject O1', 'Teacher': 'Teacher 7', 'Class': '1. Sem A Elektronik'},
+                        {'Course': 'Subject D1', 'Teacher': 'Teacher 7', 'Class': '1. Sem B Netv\xc3\xa6rk'},
+                        {'Course': 'Subject G1', 'Teacher': 'Teacher 7', 'Class': '1. Sem B Netv\xc3\xa6rk'},
+                        {'Course': 'Subject H1', 'Teacher': 'Teacher 7', 'Class': '1. Sem B Netv\xc3\xa6rk'},
+                        {'Course': 'Subject T1', 'Teacher': 'Teacher 7', 'Class': '1. Sem B Netv\xc3\xa6rk'}
+                    ]
+Teacher1FirstClass = Teacher1Classes[0]
+Teacher2Initials = 'Teacher2'
+Teacher2FirstClass = {'Course': 'Subject B1', 'Teacher': 'Teacher2', 'Class': '1. Sem A Elektronik'}
+
 class Test(unittest.TestCase):
 
     def testConstruction(self):
@@ -16,17 +29,39 @@ class Test(unittest.TestCase):
         tfi = TfCsvImport(TfInputCsvFile )
         self.assertEqual( tfi.getCsvFilename(), TfInputCsvFile )
 
-#    def testReadFromCsv(self):
-#        ''' TfImport : '''
-#        myLoader = loadWeb.htmlGetter()
-#        self._htmlResponse = myLoader.getSkemaWithPost(3735, 43, 43)
-#        parser = BeautifulSkemaScraper(DateFormat = "%d-%m-%Y")
-#        parser.feed( self._htmlResponse.read() )
-#        parser.close()
-#        i = len(parser.Appointments)
-#        self.assertEqual(i,19)
-#        pass
-#    
+    def testEnableSearchByTeacher(self):
+        ''' test selecting teacher based search '''
+        tfi = TfCsvImport(TfInputCsvFile )
+        tfi.EnableImportByTeacher(Teacher1Initials)
+        self.assertEqual( tfi.IsSearchEnabled(), True )
+
+    def testGetNextEntry(self):
+        ''' test the retrieval of the first entry (Teacher1) '''
+        tfi = TfCsvImport(TfInputCsvFile )
+        tfi.EnableImportByTeacher(Teacher1Initials)        
+        self.assertEqual( tfi.GetNextEntry(), Teacher1FirstClass )
+
+    def testGetNextEntryWithDiffTeacher(self):
+        ''' test the retrieval of the first entry (teacher2)'''
+        tfi = TfCsvImport(TfInputCsvFile )
+        tfi.EnableImportByTeacher(Teacher2Initials)        
+        self.assertEqual( tfi.GetNextEntry(), Teacher2FirstClass )
+
+    def testRestartSearch(self):
+        ''' test restarting search with new teacher '''
+        tfi = TfCsvImport(TfInputCsvFile )
+        tfi.EnableImportByTeacher(Teacher1Initials)        
+        self.assertEqual( tfi.GetNextEntry(), Teacher1FirstClass )
+        tfi.EnableImportByTeacher(Teacher2Initials)        
+        self.assertEqual( tfi.GetNextEntry(), Teacher2FirstClass )
+
+    def testGetNextEntryMultipleTimes(self):
+        ''' test the retrieval of the mulitple entries '''
+        tfi = TfCsvImport(TfInputCsvFile )
+        tfi.EnableImportByTeacher(Teacher1Initials)        
+            
+        for i in range(0, len(Teacher1Classes )):
+            self.assertEqual( tfi.GetNextEntry(), Teacher1Classes[i] )
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testPrintWebPage']
