30cee2caa9b48cdf80f74c0365bc126bb0275767
morten <morten@leon.lan>
2010-11-14 23:44:11 +0100

    DaluSkemaScraper now functioning with html files

--- src/Input/HtmlScraper/DalumSkemaScraper.py
+++ src/Input/HtmlScraper/DalumSkemaScraper.py
@@ -4,6 +4,9 @@ Created on Nov 14, 2010
 @author: morten
 '''
 
+import BeautifulSoup, datetime
+from TableIterator import TableIterator
+
 class DalumSkemaScraper():
     '''
     Get the schedule information from the dalum homepage system.
@@ -19,6 +22,9 @@ class DalumSkemaScraper():
         self._Source = "Web"
         self._HtmlData = ""
         self._Appointments = []
+        self._WeekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        self._Dates = {}
+        self._DateFormat = "%d-%m-%y"
         
     def GetId(self):
         ''' @return The internal Id used '''
@@ -38,13 +44,76 @@ class DalumSkemaScraper():
         self._Source = "HTML"
         
     def GetHtml(self):
-        ''' @return The data in the html buffer '''
+        ''' @return The data in the HTML buffer '''
         return self._HtmlData
+    
+    def GetDates(self):
+        ''' @return The dates extracted from html '''
+        return self._Dates
+    
+    def GetAppointments(self):
+        ''' @return the extracted appointments '''
+        return self._Appointments
     
     def ExtractAppointments(self):
         ''' Starts the parsing
         @return The number of appointments extracted '''
+        ti = TableIterator( self._HtmlData )
+
+        self._Lessons = []
+        IsFirstRow = True
+        for row in TableIterator( self._HtmlData ):
+            if IsFirstRow:
+                self._ProcessFirstRow( row )
+                IsFirstRow = False
+                continue
+            try:
+                self._Lessons.append( self._ProcessLessons( row ) )
+            except ValueError:
+                continue
+        
+        self._ConvertToApp()
         return len( self._Appointments )
     
-    
+    def _ProcessFirstRow(self, Row):
+        ''' First row is special. It contains the dates. '''
+        
+        for i in range(1,6):
+            self._Dates[self._WeekDays[i-1]] = datetime.datetime.strptime( Row[i].split(" ")[1], self._DateFormat )
+
+    def _ProcessLessons(self, Row):
+        ''' Lessons are retrieved horizontally '''
+        Result = {}
+
+        # lessons start and end times
+        Time = Row[0].split('-')
+        if len(Time) != 2:
+            raise ValueError("Failed to convert time. End-of-list?")
+        Result['LessonStart'] = Time[0].split(":")
+        Result['LessonEnd'] = Time[1].split(":")
+        
+        for i in range(0,5):
+            Result[self._WeekDays[i]] = (Row[3*i+1], Row[3*i+2], Row[3*i+3])
+        
+        return Result
+
+    def _ConvertToApp(self):
+        ''' converts self._Dates and _Lessons to appointments '''
+        self._Appointments = []
+        SkipList = [u' .']
+        for WeekDay in self._WeekDays:
+            for Lesson in self._Lessons:
+                if Lesson[WeekDay][0] not in SkipList:
+                    LessonStart = self._Dates[WeekDay] \
+                        + datetime.timedelta( hours=int(Lesson['LessonStart'][0]), minutes=int(Lesson['LessonStart'][1]))
+                    LessonEnd = self._Dates[WeekDay] \
+                        + datetime.timedelta( hours=int(Lesson['LessonEnd'][0]), minutes=int(Lesson['LessonEnd'][1]))
+                    self._Appointments.append( {    
+                                "Date": self._Dates[WeekDay],
+                                "Hours": [LessonStart, LessonEnd], 
+                                "Subject": Lesson[WeekDay][0],
+                                "Class": Lesson[WeekDay][1],
+                                "Location": Lesson[WeekDay][2]
+                            } )
+
     
--- src/testpackage/Input/testHtmlScraper/testDalumeSkemaScraper.py
+++ src/testpackage/Input/testHtmlScraper/testDalumeSkemaScraper.py
@@ -3,13 +3,42 @@ Created on Nov 14, 2010
 
 @author: morten
 '''
-import unittest
+import unittest, datetime
 from Input.HtmlScraper.DalumSkemaScraper import DalumSkemaScraper
 
 # test data
 DalumDataFile = "Dalum/Merete_2010_35.htm"
 DalumData = open(DalumDataFile, 'r').read()
 DalumDataAppCount = 21
+DalumDataDates = {   'Monday': datetime.datetime(2010, 8, 30, 0, 0),
+    'Tuesday': datetime.datetime(2010, 8, 31, 0, 0), 
+    'Wednesday': datetime.datetime(2010, 9, 1, 0, 0), 
+    'Thursday': datetime.datetime(2010, 9, 2, 0, 0), 
+    'Friday': datetime.datetime(2010, 9, 3, 0, 0) }
+
+DalumDataAppointment = [   
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 9, 5), datetime.datetime(2010, 8, 31, 9, 50)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 10, 10), datetime.datetime(2010, 8, 31, 10, 55)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 11, 0), datetime.datetime(2010, 8, 31, 11, 45)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 12, 30), datetime.datetime(2010, 8, 31, 13, 15)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 13, 20), datetime.datetime(2010, 8, 31, 14, 5)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 14, 25), datetime.datetime(2010, 8, 31, 15, 10)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 15, 15), datetime.datetime(2010, 8, 31, 16, 0)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 9, 1, 0, 0), 'Hours': [datetime.datetime(2010, 9, 1, 12, 30), datetime.datetime(2010, 9, 1, 13, 15)], 'Location': u'D6', 'Class': u'JHD6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 9, 1, 0, 0), 'Hours': [datetime.datetime(2010, 9, 1, 13, 20), datetime.datetime(2010, 9, 1, 14, 5)], 'Location': u'D6', 'Class': u'JHD6', 'Subject': u'Introduktion'}, 
+    {'Date': datetime.datetime(2010, 9, 1, 0, 0), 'Hours': [datetime.datetime(2010, 9, 1, 14, 25), datetime.datetime(2010, 9, 1, 15, 10)], 'Location': u'D6', 'Class': u'JHD6', 'Subject': u'HU/PL speciale'}, 
+    {'Date': datetime.datetime(2010, 9, 1, 0, 0), 'Hours': [datetime.datetime(2010, 9, 1, 15, 15), datetime.datetime(2010, 9, 1, 16, 0)], 'Location': u'D6', 'Class': u'JHD6', 'Subject': u'HU/PL speciale'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 8, 15), datetime.datetime(2010, 9, 2, 9, 0)], 'Location': u'F6', 'Class': u'J1F6', 'Subject': u'EDB'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 9, 5), datetime.datetime(2010, 9, 2, 9, 50)], 'Location': u'F5', 'Class': u'J1F5', 'Subject': u'EDB'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 12, 30), datetime.datetime(2010, 9, 2, 13, 15)], 'Location': u'KE', 'Class': u'GRFE', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 13, 20), datetime.datetime(2010, 9, 2, 14, 5)], 'Location': u'KE', 'Class': u'GRFE', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 14, 25), datetime.datetime(2010, 9, 2, 15, 10)], 'Location': u'KD', 'Class': u'GRFD', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 2, 0, 0), 'Hours': [datetime.datetime(2010, 9, 2, 15, 15), datetime.datetime(2010, 9, 2, 16, 0)], 'Location': u'KD', 'Class': u'GRFD', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 3, 0, 0), 'Hours': [datetime.datetime(2010, 9, 3, 8, 15), datetime.datetime(2010, 9, 3, 9, 0)], 'Location': u'KD', 'Class': u'GRFD', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 3, 0, 0), 'Hours': [datetime.datetime(2010, 9, 3, 9, 5), datetime.datetime(2010, 9, 3, 9, 50)], 'Location': u'KD', 'Class': u'GRFD', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 3, 0, 0), 'Hours': [datetime.datetime(2010, 9, 3, 10, 10), datetime.datetime(2010, 9, 3, 10, 55)], 'Location': u'KE', 'Class': u'GRFE', 'Subject': u'Husdyrbiologi'}, 
+    {'Date': datetime.datetime(2010, 9, 3, 0, 0), 'Hours': [datetime.datetime(2010, 9, 3, 11, 0), datetime.datetime(2010, 9, 3, 11, 45)], 'Location': u'KE', 'Class': u'GRFE', 'Subject': u'Husdyrbiologi'}] 
+
 
 class Test(unittest.TestCase):
 
@@ -30,7 +59,10 @@ class Test(unittest.TestCase):
         ''' Extract the appointments from the tests data file '''
         s = DalumSkemaScraper( 1427 )
         s.SetHtml( DalumData )
+        s.ExtractAppointments()
+        self.assertEqual( s.GetDates(), DalumDataDates )
         self.assertEqual( s.ExtractAppointments(), DalumDataAppCount )
+        self.assertEqual( s.GetAppointments(), DalumDataAppointment)
 
         
 if __name__ == "__main__":
