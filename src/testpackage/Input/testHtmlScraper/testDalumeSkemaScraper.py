7c4c3d72e74eaa115043f350c93b8f5faac834db
morten <morten@leon.lan>
2010-11-15 16:11:19 +0100

    Added multiweek tests.

--- src/testpackage/Input/testHtmlScraper/testDalumeSkemaScraper.py
+++ src/testpackage/Input/testHtmlScraper/testDalumeSkemaScraper.py
@@ -11,14 +11,15 @@ DalumDataFile = "Dalum/Merete_2010_35.htm"
 DalumId = 1427
 DalumWeek = 35
 DalumData = open(DalumDataFile, 'r').read()
-DalumDataAppCount = 21
-DalumDataDates = {   'Monday': datetime.datetime(2010, 8, 30, 0, 0),
+DalumDataAppCount_Week34 = 19
+DalumDataAppCount_Week35 = 21
+DalumDataDates_Week35 = {   'Monday': datetime.datetime(2010, 8, 30, 0, 0),
     'Tuesday': datetime.datetime(2010, 8, 31, 0, 0), 
     'Wednesday': datetime.datetime(2010, 9, 1, 0, 0), 
     'Thursday': datetime.datetime(2010, 9, 2, 0, 0), 
     'Friday': datetime.datetime(2010, 9, 3, 0, 0) }
 
-DalumDataAppointment = [   
+DalumDataAppointment_Week35 = [   
     {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 9, 5), datetime.datetime(2010, 8, 31, 9, 50)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
     {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 10, 10), datetime.datetime(2010, 8, 31, 10, 55)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
     {'Date': datetime.datetime(2010, 8, 31, 0, 0), 'Hours': [datetime.datetime(2010, 8, 31, 11, 0), datetime.datetime(2010, 8, 31, 11, 45)], 'Location': u'F5F6', 'Class': u'J1F5J1F6', 'Subject': u'Introduktion'}, 
@@ -63,9 +64,9 @@ class Test(unittest.TestCase):
         s = DalumSkemaScraper( DalumId )
         s.SetHtml( DalumData )
         s.ExtractAppointments()
-        self.assertEqual( s.GetDates(), DalumDataDates )
-        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount )
-        self.assertEqual( s.GetAppointments(), DalumDataAppointment)
+        self.assertEqual( s.GetDates(), DalumDataDates_Week35 )
+        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount_Week35 )
+        self.assertEqual( s.GetAppointments(), DalumDataAppointment_Week35)
 
     def testGetFromWebWithoutWeek(self):
         ''' DalumSkemaScraper : test failure to retrieve html from internet '''
@@ -82,17 +83,18 @@ class Test(unittest.TestCase):
         ''' DalumSkemaScraper : test correct retrieval of html from internet '''
         s = DalumSkemaScraper( DalumId, [DalumWeek]  )
         s.ExtractAppointments()
-        self.assertEqual( s.GetDates(), DalumDataDates )
-        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount )
-        self.assertEqual( s.GetAppointments(), DalumDataAppointment)
+        self.assertEqual( s.GetDates(), DalumDataDates_Week35 )
+        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount_Week35 )
+        self.assertEqual( s.GetAppointments(), DalumDataAppointment_Week35)
         
-#    def testGetMultiWeekFromWeb(self):
-#        ''' DalumSkemaScraper : test correct retrieval of html from internet '''
-#        s = DalumSkemaScraper( DalumId, [DalumWeek, DalumWeek+1]  )
-#        s.ExtractAppointments()
-#        self.assertEqual( s.GetDates(), DalumDataDates )
-#        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount )
-#        self.assertEqual( s.GetAppointments(), DalumDataAppointment)
+    def testGetMultiWeekFromWeb(self):
+        ''' DalumSkemaScraper : test correct multiweek retrieval of html from internet '''
+        s = DalumSkemaScraper( DalumId, [DalumWeek-1, DalumWeek]  )
+        s.ExtractAppointments()
+        self.assertEqual( s.GetDates(), DalumDataDates_Week35 )
+        self.assertEqual( s.ExtractAppointments(), DalumDataAppCount_Week35+DalumDataAppCount_Week34 )
+        # ought to the following also...
+        #self.assertEqual( s.GetAppointments(), DalumDataAppointment_Week35+DalumDataAppointment_Week35)
                  
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testConstructor']
