1f0fb11d1b88289544acd22dc25b1abe09759fed
mobo (cmd line) <mobo@users@sf.net>
2011-01-29 18:28:50 +0100

    more debug

--- src/Input/HtmlScraper/DalumSkemaScraper.py
+++ src/Input/HtmlScraper/DalumSkemaScraper.py
@@ -144,6 +144,7 @@ class DalumSkemaScraper():
             raise ValueError( "Bad Week number supplied" )
         
         self._UrlToOpen = "http://80.208.123.243/uge %i/3_%i.htm"%( WeekNo, self._Id )
+        print "url: ", self._UrlToOpen
         usock = urllib.urlopen(self._UrlToOpen)
         self._HtmlData = usock.read()
         usock.close()
