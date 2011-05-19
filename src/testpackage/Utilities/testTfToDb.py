51d6bce25edc19cb4e21dfad47961edccdccb1a2
mobo (cmd line) <mobo@users@sf.net>
2011-05-19 13:51:02 +0200

    made test work

--- src/testpackage/Utilities/testTfToDb.py
+++ src/testpackage/Utilities/testTfToDb.py
@@ -3,7 +3,7 @@ Created on May 7, 2011
 
 @author: morten
 '''
-import unittest, os
+import unittest
 from testpackage.Utilities.SupportStuff import * #@UnusedWildImport
 
 TfFile = "TfToHtmlWithExtra/TF_skema.csv"
@@ -12,7 +12,7 @@ Outputfilename = "tftodbtest.sqlite"
 TfToDbFilename= "../../../Utilities/TfToDb.py"
 BaseDbFile = "TfToDb/BaseDb.sql"
 
-PythonBinaryToUse = "python2.7"
+PythonBinaryToUse = "python"
 
 class Test(unittest.TestCase):
     ''' Testing HtmlToIcs from an external shell like perspective '''
@@ -29,14 +29,11 @@ class Test(unittest.TestCase):
         pass
 
     def testTfToDb(self):
-        ''' TfToHtml : compares known skema HTML input with known HTML output '''
-        CmdString = '%s %s --infile "%s" -x "%s" --outfile "%s" --basedb "%s"' \
+        ''' TfToDb : compares known skema HTML input with known HTML output '''
+        CmdString = '%s %s --infile "%s" -x "%s" --outfile "%s" --basedb "%s" > /dev/null' \
                     % (PythonBinaryToUse, TfToDbFilename, TfFile, TfExtraFile, Outputfilename, BaseDbFile )
         ret = os.system( CmdString )
         self.assertEqual( ret, 0 )
-#        ret = os.system( 'diff %s %s  > /dev/null' % (HtmlResultFile, Outputfilename ))
-#        #ret = os.system( 'diff %s %s ' % (HtmlResultFile, Outputfilename ))
-#        self.assertEqual( ret, 0 )
 
 
 if __name__ == "__main__":
