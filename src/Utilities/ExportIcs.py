567fedf4b207e1dfc2ff660927521f6cef6444ea
mobo (cmd line) <mobo@users@sf.net>
2011-12-29 23:27:13 +0100

    Implemented ExportIcs

---
+++ src/Utilities/ExportIcs.py
@@ -0,0 +1,47 @@
+'''
+Created on Dec 29, 2011
+
+@author: morten
+'''
+
+from Configuration.CommandLineOptions import ReadOptions
+import sys
+from Output.IcsOutput.IcsOutput import IcsOutput
+from Datatypes.EventFunctions import ReadEvent
+
+def ExportIcs( config, ConfigSet = "ExportIcs" ):
+    ''' Configuration items needed are the same as for ReadEvent, and
+    @param Config: the configuration object to use
+    @param ConfigSet: The sub set of the configuration object.
+    '''
+    Events = []
+    for line in sys.stdin.readlines():
+        Event = ReadEvent(line, config, ConfigSet)
+        if Event is not None:
+            Events.append( Event )
+        
+    io = IcsOutput( Events )
+
+    # save to file or stdout as specified in the config.
+    outfile = config.get( ConfigSet, 'OutputFile' )
+    sys.stderr.write( "ExportIcs output to file: %s" % ( outfile ,) )
+    f = open( outfile, "wb" )
+    f.write( io.GetIcsString() )
+    f.close()
+    
+if __name__ == '__main__':
+    # TODO: implement an argument switch to handle changing stdin to read from file
+    sys.stdin = file('../testpackage/Utilities/testdata/SdeSkemaEventData.txt')
+    
+    # 1) read config/parameter
+    # none yet
+
+    # 2) receive config
+    config = ReadOptions( Silent = True)
+
+    # 3) print config
+    # PrintConfig( config )
+        
+    # 4) receive events and output them
+    ExportIcs( config )
+    
