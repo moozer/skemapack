5e8ae1598de3784f9a9222cdebae854f9e5e4b94
moz <moz@leon.lan>
2012-01-28 17:05:22 +0100

    Changed ExportIcs to handle config and events correctly added tests

--- src/Export/ExportIcs.py
+++ src/Export/ExportIcs.py
@@ -12,27 +12,18 @@ import sys
 from Output.IcsOutput.IcsOutput import IcsOutput
 from Datatypes.EventFunctions import ReadEvent
 
-def ExportIcs( config, ConfigSet = "ExportIcs" ):
+def ExportIcs( Events, config, ConfigSet = "ExportIcs" ):
     ''' Configuration items needed are the same as for ReadEvent() 
         and 'OutputFile', 'InputFile'
+    @param Events: the events to output 
     @param Config: the configuration object to use
     @param ConfigSet: The sub set of the configuration object.
     '''
-    Events = []
-    
-    infile = config.get( ConfigSet, 'InputFile' )
-    sys.stderr.write( "ExportIcs imput from file: %s\n" % ( infile ,) )
-    fin = open( infile, "r")
-        
-    for line in fin.readlines():
-        Event = ReadEvent(line, config, ConfigSet)
-        if Event is not None:
-            Events.append( Event )
         
     io = IcsOutput( Events )
 
     # save to file or stdout as specified in the config.
-    outfile = config.get( ConfigSet, 'OutputFile' )
+    outfile = config.get( ConfigSet, 'OutFile' )
     sys.stderr.write( "ExportIcs output to file: %s\n" % ( outfile ,) )
     f = open( outfile, "wb" )
     f.write( io.GetIcsString() )
@@ -52,5 +43,16 @@ if __name__ == '__main__':
     # PrintConfig( config )
         
     # 4) receive events and output them
+    Events = []
+    
+    infile = config.get( ConfigSet, 'InputFile' )
+    sys.stderr.write( "ExportIcs imput from file: %s\n" % ( infile ,) )
+    fin = open( infile, "r")
+        
+    for line in fin.readlines():
+        Event = ReadEvent(line, config, ConfigSet)
+        if Event is not None:
+            Events.append( Event )
+
     ExportIcs( config )
     
---
+++ src/testpackage/Export/testExportIcs.py
@@ -0,0 +1,47 @@
+'''
+Created on 28 Jan 2012
+
+@author: moz
+'''
+import unittest
+from testpackage.Utilities.SupportStuff import * #@UnusedWildImport
+from Configuration.SkemaPackConfig import SkemaPackConfig
+from Export.ExportIcs import ExportIcs
+from Import.ImportFile import ImportFile
+
+ImportFileWorkDir = 'ExportIcs'
+ImportFileCfgFilename = 'ExportIcs.cfg'
+
+IcsFilename = 'ExportIcsResult.ics'
+IcsKnownResultFile = 'ExportIcsKnownResult.ics'
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        CloneTestData() 
+        self._StartDir = ChDirToSrc()
+        os.chdir(TempDataDir)
+        os.chdir( ImportFileWorkDir)
+        
+        self.myConfig = SkemaPackConfig( open( ImportFileCfgFilename ) )
+        pass
+
+    def tearDown(self):
+        ''' Removes temporary data '''
+        RemoveTestData()        
+        os.chdir(self._StartDir )
+        pass
+
+
+    def testName(self):
+        ''' ExportIcs : exporting known entries '''
+        events = ImportFile( self.myConfig )
+        ExportIcs( events, self.myConfig )
+        
+        ret = os.system( 'diff %s %s ' % (IcsFilename, IcsKnownResultFile ))
+        self.assertEqual( ret, 0 )
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
