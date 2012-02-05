e18fe63fd6472323e25c4536afe213f369d07f14
moz <moz@leon.lan>
2012-02-05 10:57:07 +0100

    Export file now handle None as config object

--- src/Export/ExportFile.py
+++ src/Export/ExportFile.py
@@ -8,19 +8,32 @@ Created on 28 Jan 2012
 '''
 
 from Datatypes.EventFunctions import WriteEvents, ReadEvent, MakeEventString
-from Configuration.SkemaPackConfig import SkemaPackConfig,SkemaPackConfig_stdin
+#from Configuration.SkemaPackConfig import SkemaPackConfig,SkemaPackConfig_stdin
 import sys
 
    
-def ExportFile( Events, config, ConfigSet = "ExportFile" ):
+def ExportFile( Events, config = None, ConfigSet = "ExportFile" ):
     # read data from file or net
-    fp = open(config.get(ConfigSet, "Outfile"), 'w')
+    if not config:
+        fp = sys.stdout
+        DateFormat = "%Y-%m-%d"
+    else:
+        # get output file name
+        if config.has_option(ConfigSet, "Outfile"):
+            fp = open(config.get(ConfigSet, "Outfile"), 'w')
+        else:
+            fp = sys.stdout
 
+        # read data from file or net
+        if not config.has_option(ConfigSet, "OutputDateformat"):
+            DateFormat = "%Y-%m-%d"
+        else:
+            DateFormat = config.get(ConfigSet, "OutputDateformat")
+
+    # looping over events and outputting
     for event in Events:
-        fp.write(MakeEventString(event, config.get(ConfigSet, "OutputDateformat")) )
+        fp.write(MakeEventString(event, DateFormat ) )
 
-    fp.close()
-    return
 
 #if __name__ == '__main__':
 #
--- src/testpackage/Export/testExportToFile.py
+++ src/testpackage/Export/testExportToFile.py
@@ -9,8 +9,11 @@ from Configuration.SkemaPackConfig import SkemaPackConfig
 from Import.ImportFile import ImportFile
 from Export.ExportFile import ExportFile
 from testpackage.Import.testImportFile import ImportFileData, ImportFileWorkDir, ImportFileCfgFilename
+import sys
 
 # using the same vars as testImportFile
+
+ExportFileTestFileName = "teststdout.txt"
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -29,9 +32,20 @@ class Test(unittest.TestCase):
         pass
 
     def testBasicExportAndImport(self):
+        ''' ExportFile : basic write+read test '''
         ExportFile(ImportFileData, self.myConfig )
         Events = ImportFile(self.myConfig, 'ImportExportedFile')
         self.assertEqual( Events, ImportFileData )
+        pass
+    
+    def testConfigObjectNone(self):
+        ''' ExportFile : test if ExportFile can handle None as config object '''
+        old_stdout = sys.stdout
+        fp = file( ExportFileTestFileName, 'w+' )
+        sys.stdin = fp 
+        ExportFile( ImportFileData ) # exprot using defaults
+        sys.stdout = old_stdout
+        fp.close()
         pass
 
 
