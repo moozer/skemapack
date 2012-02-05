e9eb0695eb2a57136e62996d5897b4559a08b545
moz <moz@leon.lan>
2012-02-05 10:57:58 +0100

    ImportFile.py is now a good example of an ImportXX __main__

--- src/Import/ImportFile.py
+++ src/Import/ImportFile.py
@@ -7,9 +7,9 @@ Created on 28 Jan 2012
 @author: moz
 '''
 from Datatypes.EventFunctions import WriteEvents, ReadEvent
-from Configuration.SkemaPackConfig import SkemaPackConfig,SkemaPackConfig_stdin
+from Configuration.SkemaPackConfig import SkemaPackConfig
 import sys
-
+from Export.ExportFile import ExportFile
 
 def ImportFile( config = None, ConfigSet = "ImportFile" ):
     # if no config supplied, use defaults
@@ -24,7 +24,7 @@ def ImportFile( config = None, ConfigSet = "ImportFile" ):
             FileToUse = open( config.get(ConfigSet, "Infile") )
 
         # read data from file or net
-        if not config.has_option(ConfigSet, "Infile"):
+        if not config.has_option(ConfigSet, "InputDateformat"):
             DateFormat = "%Y-%m-%d"
         else:
             DateFormat = config.get(ConfigSet, "InputDateformat")
@@ -43,20 +43,20 @@ def ImportFile( config = None, ConfigSet = "ImportFile" ):
 
 
 if __name__ == '__main__':
-
+    # initial vars
+    Module = "ImportFile"
+    ConfigSet = "SkemaScraper"
+    
+    # handle command line config file (if set)
     if len(sys.argv) > 2:
         config = SkemaPackConfig( sys.argv[1] )
+        sys.stderr.write( "%s : config file is %s\n"%(Module, config.name))
     else:
         config = None
+        sys.stderr.write( "%s : config from stdin\n"%Module)
         
-    sys.stderr.write( "ImportFile : config file is %s\n"%cfgfile)
-
-#    # 1) read config/parameter
-    ConfigSet = "SkemaScraper"
-
-    # 3) import from skema
+    # import from file
     Events = ImportFile( config, ConfigSet )
     
-    # 4) output all events to stdout
-    print config # placed here to allow config to be changed...
-    WriteEvents( Events, config, ConfigSet )
+    # output to file
+    ExportFile( Events, config, ConfigSet )
