8d83dd680d2145be499b7cd9bc606bbf7c218506
moz <moz@leon>
2012-04-04 15:43:34 +0200

    made ImportTfZip runable setting config option correctly for ImportTf

--- src/Import/ImportTfZip.py
+++ src/Import/ImportTfZip.py
@@ -11,6 +11,7 @@ from Configuration.SkemaPackConfig import SkemaPackConfig, SkemaPackConfig_stdin
 import sys
 from Export.ExportFile import ExportFile
 from zipfile import ZipFile
+from Configuration.SkemaPackConfig import SkemaPackConfig,SkemaPackConfig_stdin
 
 
 def ImportTfZip( config = None, ConfigSet = "ImportFile" ):
@@ -19,23 +20,39 @@ def ImportTfZip( config = None, ConfigSet = "ImportFile" ):
     @param config: The config object to use. If None, then we try to use defaults or stdin
     @param ConfigSet: The section of the configuration to use
     @raise KeyError: If supplied ConfigSet is not in config
-    @return: (events, config) or (weeksums, config) 
+    @return: (weeksums, config) 
     '''  
 
     # fetch config values
     ZipDataDir = config.get( ConfigSet, "ZipDataDir" )
     ZipFilename = config.get( ConfigSet, "ZipFile" )
 
-    ZipFile( ZipFilename ).extractall( ZipDataDir )
-    
-#    mbers, pwd)
-#    TfZip = zipfile.ZipFile(zipfile_new)
-#    print "files in archive"
-#    files = TfZip.namelist()
-#    print files
-#    TfZip.extractall(Directories['Raw'])
-#    return files
+    zf = ZipFile( ZipFilename )
+    zf.extractall( ZipDataDir )
 
+    ImportTfSection = "ImportTf" 
+    for DataFile in zf.namelist():    
+        config.set( ImportTfSection, "Infile", DataFile )
+        print "%s"%config
     
     return [], config
 
+
+if __name__ == '__main__':
+
+    # allow cfg file from cmd line
+    if len(sys.argv) > 1:
+        cfgfile = open( sys.argv[1] )
+    else:
+        cfgfile = SkemaPackConfig_stdin()
+
+    # 1) read config/parameter
+    config = SkemaPackConfig( cfgfile )
+    ConfigSet = "ImportTfZip"
+
+    # 3) import from skema
+    Events = ImportTfZip( config, ConfigSet )
+    
+    # 4) output all events to stdout
+    ExportFile( Events, config, ConfigSet )
+    
