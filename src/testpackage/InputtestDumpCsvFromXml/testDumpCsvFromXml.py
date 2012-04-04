b1d9c901a6bfabeaa32da0f442ec07f71a661310
flindt <flindt@flindt-X301>
2012-04-04 14:35:34 +0200

    added testdate for DumpCsv

--- src/testpackage/InputtestDumpCsvFromXml/testDumpCsvFromXml.py
+++ src/testpackage/InputtestDumpCsvFromXml/testDumpCsvFromXml.py
@@ -6,10 +6,36 @@ Created on Mar 29, 2012
 import unittest
 
 from Input.DumpCsv import DumpCsvFromXml
+from Input import DumpCsv
+import filecmp
+from Configuration import SkemaPackConfig
+import os
+
+from Import.ImportFile import ImportFile
+from Export.ExportHtml import ExportHtml
+
+from testpackage.Utilities.SupportStuff import * #@UnusedWildImport
+from Configuration.SkemaPackConfig import SkemaPackConfig
+
+ ExportHtmlWorkDir = "DumpCsv"
  
 
 class Test(unittest.TestCase):
 
+    def setUp(self):
+        CloneTestData() 
+        self._StartDir = ChDirToSrc()
+        os.chdir(TempDataDir)
+        os.chdir( ExportHtmlWorkDir )
+        
+        #self.myConfig = SkemaPackConfig( open( ExportHtmlCfgFile  ) )
+        pass
+
+    def tearDown(self):
+        ''' Removes temporary data '''
+        RemoveTestData()        
+        os.chdir(self._StartDir )
+        pass
 
     def testNoInput(self):
         XmlFileName = None 
@@ -17,6 +43,8 @@ class Test(unittest.TestCase):
         SheetName = None
         Seperator = None
         DumpCsvFromXml.DumpNamedSheet( XmlFileName, CsvFileName, SheetName, Seperator)
+        
+        
         pass
 
 
