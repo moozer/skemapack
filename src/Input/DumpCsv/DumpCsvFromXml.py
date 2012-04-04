116fa68fe57fc7c0396fca0bd167320b552745f7
flindt <flindt@flindt-X301>
2012-04-04 15:08:29 +0200

    DumpCsvFromXml now calls openoffice "libreoffice"

--- src/Input/DumpCsv/DumpCsvFromXml.py
+++ src/Input/DumpCsv/DumpCsvFromXml.py
@@ -3,11 +3,18 @@ Created on Mar 29, 2012
 
 @author: flindt
 '''
+import os
 
 
 
 def DumpNamedSheet(XmlFileName, CsvFileName, SheetName, Seperator):
     
+    
+    Cmd = "libreoffice"
+    Cmd = '%s %s --headless --norestore "--accept=socket,host=localhost,port=2002;urp;"'%(Cmd, "\"%s\""%XmlFileName)
+       
+    print "issuing: %s"%Cmd
+    os.system( Cmd )
     
     pass
 
